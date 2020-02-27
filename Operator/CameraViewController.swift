198b70ce352f75efd556790a88d1869061b85621
Henning Hovland <henning@dekode.no>
2020-02-27 23:31:32 +0100

    Added Rocc Library and added tests

---
+++ Operator/CameraViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraViewController.swift
+//  Operator
+//
+//  Created by Henning Hovland on 27/02/2020.
+//  Copyright © 2020 Framesby me. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
